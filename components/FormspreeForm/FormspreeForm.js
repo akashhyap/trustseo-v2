62ab39feebf24a40cffe42beed6cab73fbbad8aa
Akashdeep <akash.kashhyap@gmail.com>
2022-10-31 11:45:57 +0530

    form submit button ui update

--- components/FormspreeForm/FormspreeForm.js
+++ components/FormspreeForm/FormspreeForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
+import { FaArrowRight } from "react-icons/fa";
 
 export const FormspreeForm = ({ formId }) => {
   const [state, handleSubmit] = useForm(formId);
@@ -10,11 +11,8 @@ export const FormspreeForm = ({ formId }) => {
     <form onSubmit={handleSubmit} className="form_wrap">
       <div className="grid grid-cols-6 gap-6 my-5">
         <div className="col-span-6 sm:col-span-3">
-          <label
-            htmlFor="fname"
-            className="block text-sm text-gray-700"
-          >
-           First Name
+          <label htmlFor="fname" className="block text-sm text-gray-700">
+            First Name
           </label>
           <input
             id="fname"
@@ -24,14 +22,10 @@ export const FormspreeForm = ({ formId }) => {
             required
           />
           <ValidationError field="fname" errors={state.errors} />
-           
         </div>
         <div className="col-span-6 sm:col-span-3">
-          <label
-            htmlFor="lname"
-            className="block text-sm text-gray-700"
-          >
-           Last Name
+          <label htmlFor="lname" className="block text-sm text-gray-700">
+            Last Name
           </label>
           <input
             id="lname"
@@ -39,13 +33,9 @@ export const FormspreeForm = ({ formId }) => {
             name="lname"
             className="mt-1 block w-full rounded-md input_form"
           />
-        
         </div>
         <div className="col-span-6 sm:col-span-6">
-          <label
-            htmlFor="email"
-            className="block text-sm text-gray-700"
-          >
+          <label htmlFor="email" className="block text-sm text-gray-700">
             Email Address
           </label>
           <input
@@ -55,13 +45,9 @@ export const FormspreeForm = ({ formId }) => {
             className="mt-1 block w-full rounded-md input_form"
             required
           />
-          
         </div>
         <div className="col-span-6 sm:col-span-6">
-          <label
-            htmlFor="c_message"
-            className="block text-sm text-gray-700"
-          >
+          <label htmlFor="c_message" className="block text-sm text-gray-700">
             Message
           </label>
           <textarea
@@ -79,9 +65,12 @@ export const FormspreeForm = ({ formId }) => {
       <button
         type="submit"
         disabled={state.submitting}
-        className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        className="text-[16px] inline-flex items-center text-left bg-transparent transition-all duration-150 hover:bg-slate-800 text-white-700 hover:text-white py-4 px-2 lg:px-4 border border-white-500 hover:border-transparent rounded"
       >
         Submit
+        <span className="ml-2">
+          <FaArrowRight />
+        </span>
       </button>
       <ValidationError errors={state.errors} />
     </form>
